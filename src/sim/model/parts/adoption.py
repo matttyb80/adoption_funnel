6d5648f84093342c1dcee1c578fa5a97f581b52b
Matt Barlin <33430774+matttyb80@users.noreply.github.com>
2020-05-23 15:21:52 -0400

    second commit

---
+++ src/sim/model/parts/adoption.py
@@ -0,0 +1,31 @@
+import networkx as nx
+import numpy as np
+
+from ..utils import *
+
+def p_reputation(params, substep, state_history, prev_state):
+    """
+    Policy for steady marketing spend signal generation.
+    """
+    constant = params['THRESHOLD']
+    random = np.random.normal(params['THRESHOLD'], scale = 1.0)
+    return {'reputation': constant}
+
+
+def p_experience(params, substep, state_history, prev_state):
+    """
+    Policy for steady marketing spend signal generation.
+    """
+    constant = params['EXO_EXPERIENCE']
+    random = np.random.normal(params['EXO_EXPERIENCE'], scale = 1.0)
+    return {'experience': constant}
+
+
+def s_adoption(params, substep, state_history, prev_state, policy_input):
+    """
+    State for generating signal from marketing.
+    """
+    key = 'adoption'
+
+    value = policy_input['reputation'] + policy_input['experience']
+    return (key, value)
