6111e8b56757faf6c3c75740103323ecf8e8c643
Matt Barlin <33430774+matttyb80@users.noreply.github.com>
2020-07-01 12:53:47 -0400

    cadccad3 viz threshold test

--- src/sim/run.py
+++ src/sim/run.py
@@ -7,31 +7,31 @@ import pprint
 
 ###################################
 from src.sim import config
-# from cadCAD import configs
+from cadCAD import configs
 import pandas as pd
 
 # RUN on 0.3.15
-# def run(drop_midsteps=True):
-#     exec_mode = ExecutionMode()
-#     multi_proc_ctx = ExecutionContext(context=exec_mode.multi_proc)
-#     run = Executor(exec_context=multi_proc_ctx, configs=configs)
-#     results = pd.DataFrame()
-#     i = 0
-#     for raw_result, _ in run.execute():
-#         params = configs[i].sim_config['M']
-#         result_record = pd.DataFrame.from_records([tuple([i for i in params.values()])], columns=list(params.keys()))
+def run_3(drop_midsteps=True):
+    exec_mode = ExecutionMode()
+    multi_proc_ctx = ExecutionContext(context=exec_mode.multi_proc)
+    run = Executor(exec_context=multi_proc_ctx, configs=configs)
+    results = pd.DataFrame()
+    i = 0
+    for raw_result, _ in run.execute():
+        params = configs[i].sim_config['M']
+        result_record = pd.DataFrame.from_records([tuple([i for i in params.values()])], columns=list(params.keys()))
 
-#         df = pd.DataFrame(raw_result)
-#         # keep only last substep of each timestep
-#         if drop_midsteps:
-#             max_substep = max(df.substep)
-#             is_droppable = (df.substep!=max_substep)&(df.substep!=0)
-#             df.drop(df[is_droppable].index, inplace=True)
+        df = pd.DataFrame(raw_result)
+        # keep only last substep of each timestep
+        if drop_midsteps:
+            max_substep = max(df.substep)
+            is_droppable = (df.substep!=max_substep)&(df.substep!=0)
+            df.drop(df[is_droppable].index, inplace=True)
 
-#         result_record['dataset'] = [df]
-#         results = results.append(result_record)
-#         i += 1
-#     return results.reset_index()
+        result_record['dataset'] = [df]
+        results = results.append(result_record)
+        i += 1
+    return results.reset_index()
 
 # import matplotlib.pyplot as plt
 # import seaborn as sns
@@ -72,24 +72,24 @@ def extract_params(configs,hyperparameter):
     return params
 
 
-from cadCAD.engine import ExecutionMode, ExecutionContext
-exec_mode = ExecutionMode()
-local_mode_ctx = ExecutionContext(context=exec_mode.local_mode)
-from cadCAD.engine import Executor
-from cadCAD import configs
-simulation = Executor(exec_context=local_mode_ctx, configs=configs)
-raw_system_events, tensor_field, sessions = simulation.execute()
-print(configs_as_objs(configs))
+# from cadCAD.engine import ExecutionMode, ExecutionContext
+# exec_mode = ExecutionMode()
+# local_mode_ctx = ExecutionContext(context=exec_mode.local_mode)
+# from cadCAD.engine import Executor
+# from cadCAD import configs
+# simulation = Executor(exec_context=local_mode_ctx, configs=configs)
+# raw_system_events, tensor_field, sessions = simulation.execute()
+# print(configs_as_objs(configs))
 
-# pprint(configs_as_objs(configs))
+# # pprint(configs_as_objs(configs))
 
-fmt_configs = configs_as_objs(configs)
-print(fmt_configs)
-# print()
-# pprint(fmt_configs[0].sim_config)
-# Result System Events DataFrame
-df = pd.DataFrame(raw_system_events)
-params = extract_params(configs,'THRESHOLD')
+# fmt_configs = configs_as_objs(configs)
+# print(fmt_configs)
+# # print()
+# # pprint(fmt_configs[0].sim_config)
+# # Result System Events DataFrame
+# df = pd.DataFrame(raw_system_events)
+# params = extract_params(configs,'THRESHOLD')
 
 
 
