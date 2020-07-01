8dd09892629f9dd2de09d7ae0455ccf2612758ba
Matt Barlin <33430774+matttyb80@users.noreply.github.com>
2020-07-01 14:18:37 -0400

    init for param sweep fix

--- src/sim/run.py
+++ src/sim/run.py
@@ -10,69 +10,7 @@ from src.sim import config
 # from cadCAD import configs
 import pandas as pd
 
-# RUN on 0.3.15
-# def run(drop_midsteps=True):
-#     exec_mode = ExecutionMode()
-#     multi_proc_ctx = ExecutionContext(context=exec_mode.multi_proc)
-#     run = Executor(exec_context=multi_proc_ctx, configs=configs)
-#     results = pd.DataFrame()
-#     i = 0
-#     for raw_result, _ in run.execute():
-#         params = configs[i].sim_config['M']
-#         result_record = pd.DataFrame.from_records([tuple([i for i in params.values()])], columns=list(params.keys()))
 
-#         df = pd.DataFrame(raw_result)
-#         # keep only last substep of each timestep
-#         if drop_midsteps:
-#             max_substep = max(df.substep)
-#             is_droppable = (df.substep!=max_substep)&(df.substep!=0)
-#             df.drop(df[is_droppable].index, inplace=True)
-
-#         result_record['dataset'] = [df]
-#         results = results.append(result_record)
-#         i += 1
-#     return results.reset_index()
-
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# import numpy as np
-# import pandas as pd
-# from model.parts.supportingFunctions import *
-# pd.options.display.float_format = '{:.2f}'.format
-# %matplotlib inline
-# from model import economyconfig 
-
-def extract_params(configs,hyperparameter):
-    '''
-    Description:
-    Function to extract param sweep information from simulation configuration
-    
-    Parameters:
-    configs: cadCAD configs object
-    hyperparameter: string of the hyperparameter being swept
-    
-    Assumptions:
-    Parameter sweep simulation with M as how is denoted in the sim config.
-    
-    from cadCAD.configuration.utils import configs_as_objs
-
-    Returns
-    
-    list of parameter
-    
-    Example run:
-    params = extract_params(configs,'drip_frequency')
-    '''
-
-    params = []
-    
-    for i in range(0,len(configs_as_objs(configs))):
-        params.append(configs_as_objs(configs)[i].sim_config['M'][hyperparameter])
-        
-    return params
-
-
-from cadCAD.engine import ExecutionMode, ExecutionContext
 exec_mode = ExecutionMode()
 local_mode_ctx = ExecutionContext(context=exec_mode.local_mode)
 from cadCAD.engine import Executor
@@ -125,7 +63,7 @@ def run():
     
 #     # params = configs[i].sim_config['M']
 #     df = pd.DataFrame.from_records([tuple([i for i in params.values()])], columns=list(params.keys()))
-    return df, tensor_field, sessions, params
+    return df, tensor_field, sessions
 
 # def run(drop_midsteps=True):
 #
